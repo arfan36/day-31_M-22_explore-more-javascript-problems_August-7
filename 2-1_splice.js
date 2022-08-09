1aba193244b1894a82026eadc63048f0c85049c7
arfan36 <arfanur36@gmail.com>
2022-08-09 08:09:18 +0600

    22-2 Get part of an array

---
+++ 2-1_splice.js
@@ -0,0 +1,8 @@
+const friends = [12, 14, 32, 22, 44, 62, 29, 69, 87];
+
+// Removes elements from an array and, if necessary, inserts new elements in their place, 
+// returning the deleted elements.
+// will change the original array
+const partial = friends.splice(2, 5, 99, 555, 7777);
+console.log(partial);
+console.log(friends);
